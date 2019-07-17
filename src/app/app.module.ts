d7cdcb4797e0ceacbd84dae444ba621849f58b8b
Alec 3 <43789179+AlejandroRodriguez1998@users.noreply.github.com>
2019-07-17 18:37:27 +0200

    Version 1.0.0

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import localeEs from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common';
+
+import { AppComponent } from './app.component';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+import { ContrasenaPipe } from './pipes/contrasena.pipe';
+
+registerLocaleData(localeEs, 'es');
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CapitalizadoPipe,
+    DomseguroPipe,
+    ContrasenaPipe
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es' }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
